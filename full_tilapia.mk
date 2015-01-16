e88fdb85c6b26f5dfea3bc1786387c6d144e8555
Dave Kessler <activethrasher00@gmail.com>
2015-01-16 01:50:36 -0500

    aosp_base -> full_tab_base
    fixes phone sounds being placed on tablets
    
    Change-Id: I00823c09ae4fdba3aef5a7f8411c121079f7abef

--- full_tilapia.mk
+++ full_tilapia.mk
@@ -23,7 +23,7 @@
 $(call inherit-product, device/asus/tilapia/device.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_tab_base.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_tilapia
